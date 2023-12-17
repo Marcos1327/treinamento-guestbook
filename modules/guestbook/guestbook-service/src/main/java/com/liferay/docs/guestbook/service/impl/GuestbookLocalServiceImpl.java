e0d98d6f67ed9aac3a998e6d568d1c40f2c4b00d
Marcos1327 <giovannygigi@gmail.com>
2023-12-17 14:45:25 -0300

    search e indexer added in guestbook

--- modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookLocalServiceImpl.java
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookLocalServiceImpl.java
@@ -24,6 +24,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
@@ -43,6 +45,7 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class GuestbookLocalServiceImpl extends GuestbookLocalServiceBaseImpl {
 	
+	@Indexable(type = IndexableType.REINDEX)
 	public Guestbook addGuestbook(long userId, String name,
 			ServiceContext serviceContext) throws PortalException {
 
@@ -76,6 +79,7 @@ public class GuestbookLocalServiceImpl extends GuestbookLocalServiceBaseImpl {
 		return guestbook;
 	}
 	
+	@Indexable(type = IndexableType.REINDEX)
 	public Guestbook updateGuestbook(long userId, long guestbookId,
 		    String name, ServiceContext serviceContext) throws PortalException, SystemException {
 
@@ -103,6 +107,7 @@ public class GuestbookLocalServiceImpl extends GuestbookLocalServiceBaseImpl {
 	        return guestbook;
 	}
 	
+	@Indexable(type = IndexableType.DELETE)
 	public Guestbook deleteGuestbook(long guestbookId,
             ServiceContext serviceContext) throws PortalException,
             SystemException {
